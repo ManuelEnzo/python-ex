458909eb7fe867822cd46b9894934f4969c8ca81
Manuel Enzo <107991244+codinglifewww@users.noreply.github.com>
2022-07-31 16:10:20 +0200

    Create paga_lavoratore.py
    Il salario settimanale di un dipendente viene calcolato moltiplicando la paga oraria per il numero di ore lavorate, aggiungendo il numero di ore straordinarie, pagate come 1.5 ore normali.

---
+++ paga_lavoratore.py
@@ -0,0 +1,26 @@
+
+PAGA_STR = 1.5
+
+def calcolo_paga(paga, ore):
+    print('Test 1 Passed')
+
+    x = input('Ha fatto anche straordinari: (y/n)')
+    if x == 'y':
+        ore_str = float(input('Quante ore ?: '))
+        paga_straordinaria = (paga * PAGA_STR) * ore_str
+        return int(paga * ore + paga_straordinaria)
+    else:
+        return int(paga * ore)
+
+
+
+
+
+def lavoratore():
+    paga_oraria = float(input('Paga Oraria: '))
+    numero_ore = float(input('Ore lavorate: '))
+    
+    print(calcolo_paga(paga_oraria, numero_ore), '€')
+
+
+lavoratore()
